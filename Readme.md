075ab785226fda20d26c3a62196b76fb83504bc0
Carlos Dell'Aquila <carlos.dellaquila@gmail.com>
2022-07-18 20:01:17 -0300

    Entrega final

--- Readme.md
+++ Readme.md
@@ -1,10 +1,24 @@
 # Título
 Graficador de valores de tensión.
-## Objetivo
-Un usuario autorizado puede buscar una archivo almancenado en su computadora, leerlo y graficarlo.
-Se prevee que esa gráfica sea interactiva con el usuario. Esto significa que podrá hacer zoom, descplazar a la izquierda o derecha. Incluso tendrá la opción de dibujar anotaciones y luego guardarlas.
+## General
+Mostrar por tabla y gráfico, valores de tensión que son cargados por el usuario por medio de la página web.
+Asimismo el sitio web muestra un conjunto de mediciones previas que se encuentran almacenadas en un archivo JSON.
 
-## Etapas
+La mediciones previas se obtiene por medio del método HTTP GET, los cuales se implementan por medio de promise y AJAX. Antes de ello, el usuario se debe "loggear". Los datos por defecto son:
 
-### Etapa 1
-Se realiza toda la carga de datos por medio promt y la visualización por consola.
+usr: carlos@correo.com
+pass: ingreso01
+
+Por otro lado el sitio cuenta con campos para cargar las mediciones de manuera manual. Para ello se introduce el instante de tiempo de la medición, el valor de tensión y la unidad de medida. El formato para la carga del instante es: "YYYY-MM-DDThh:mm:ss".
+
+Pero, ¿Qué tiene de interesante este sitio?
+
+El gráfico se realiza por medio de un elemento SVG (gráficos vectorizados) y código javascript. Si bien existen librerías para hacer gráfico como el que realiza el sitio web, en algunas ocaciones estos no cumplen con los requisitos del proyecto. Algunas de ellas, tienen una estructura definida para presentar las etiquetas de las escalas, lo cual genera dificultades en algunos proyectos. 
+
+Por tal motivo me resultó interesante realizar una primera aproximación que permita en un futuro realizar gráficos vectorizados y personalizados y que se generen a partir de los datos que se encuentran en un JSON.
+
+
+
+
+
+
--- js/index.js
+++ js/index.js
@@ -2,12 +2,10 @@
 const USER = "carlos@correo.com";
 const PASS = "ingreso01";
 
-
 /*
  *  Clase USUARIO: 
  *  Agrupa todos los datos y acciones referido a los usuarios.
  */
-
 class Usuario {
     // Constructor usuario
     constructor(user, pass) {
@@ -18,17 +16,7 @@ class Usuario {
     // Método para comparar usuarios. 
     verificaLogin(user, pass) {
 
-        let login = false;
-
-        if ((this.user == user) && (this.pass == pass)) {
-
-            //alert("Login exitoso!");
-            login = true;
-
-        } else {
-            //alert("Usuario o contraseña incorrecto!, vuelca a intentar.");
-            login = false;
-        }
+        let login = ((this.user == user) && (this.pass == pass)) ? true : false;
 
         return login;
     }
@@ -39,7 +27,7 @@ class Usuario {
  */
 class Medicion {
     constructor(objLiteral) {
-        this.time = objLiteral.time;   // Formato hh:mm:ss,dd/MM/YYYY.
+        this.time = new Date(objLiteral.time);
         this.value = parseFloat(objLiteral.value);  // Valor numerico.
         this.unit = objLiteral.unit;   // Unidad de medida (v, mV, uv, etc.)
     }
@@ -57,67 +45,262 @@ class Medicion {
     }
 }
 
-/* Array objeto con mediciones previas */
-const medPrevias = [
-    {
-        time: "10:00:00, 25/05/2022",
-        value: 220,
-        unit: "v",
-    },
-    {
-        time: "11:00:00, 25/05/2022",
-        value: 218,
-        unit: "v",
-    },
-    {
-        time: "12:00:00, 25/05/2022",
-        value: 230,
-        unit: "v",
-    },
-    {
-        time: "13:00:00, 25/05/2022",
-        value: 240,
-        unit: "v",
-    },
-    {
-        time: "14:00:00, 25/05/2022",
-        value: 216,
-        unit: "v",
-    },
-    {
-        time: "15:00:00, 25/05/2022",
-        value: 210,
-        unit: "v",
-    },
-    {
-        time: "16:00:00, 25/05/2022",
-        value: 220,
-        unit: "v",
+/*
+ * Clase objeto gráfico
+ */
+class Grafico {
+    constructor(objGrafico) {
+
+        /* TODO */
+        this.objGrafico = objGrafico;
+
+        /* Crea grafico en html. */
+        this.grafico = document.getElementById(objGrafico.svgID);
+        this.grafico.setAttribute('width', objGrafico.svgWidth);
+        this.grafico.setAttribute('height', objGrafico.svgHeight);
+        this.grafico.setAttribute('viewBox', objGrafico.svgViewBox);
     }
-]; 
 
+    creaAreaGrafico() {
 
-/* Lee las mediciones almacenadas, si no están carga una por defecto. */
-const medAlmacenadas = JSON.parse(localStorage.getItem("mediciones")) || medPrevias;
+        let x = (this.objGrafico.svgWidth - this.objGrafico.areaGraficoWidth) / 2;
+        let y = (this.objGrafico.svgHeight - this.objGrafico.areaGraficoHeight) / 2;
+      
+        let rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+        rect.setAttribute('x', x);
+        rect.setAttribute('y', y);
+        rect.setAttribute('width', this.objGrafico.areaGraficoWidth);
+        rect.setAttribute('height', this.objGrafico.areaGraficoHeight);
+        rect.setAttribute('stroke-width', this.objGrafico.borderWidth);
+        rect.setAttribute('stroke', 'black');
+        rect.setAttribute('fill', 'white');
+        this.grafico.append(rect);
+    }
 
-/* Arrays para almacenar los valores previos y los que serán leídos por promt. */
-const mediciones = [];
+    creaLineasVertical() {
 
-/* Cargo el arreglo previo */
-for (const med of medAlmacenadas) {
-    mediciones.push(new Medicion(med));
+        let step = (this.objGrafico.areaGraficoWidth - (2 * this.objGrafico.margenCeroX)) / (this.objGrafico.cantVerticalLines-1);
+
+        let puntoInicialEjeY = ((this.objGrafico.svgHeight - this.objGrafico.areaGraficoHeight) / 2) + (this.objGrafico.borderWidth / 2);
+        let puntoFinalEjeY = (this.objGrafico.svgHeight - this.objGrafico.areaGraficoHeight) / 2 + this.objGrafico.areaGraficoHeight - (this.objGrafico.borderWidth / 2);
+
+        let xPrimerLinea = ((this.objGrafico.svgWidth - this.objGrafico.areaGraficoWidth) / 2) + this.objGrafico.margenCeroX;
+
+        
+
+        for (let i = 0; i < this.objGrafico.cantVerticalLines; i++) {
+
+            let x = (i * step) + xPrimerLinea;
+            
+            let line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+            line.setAttribute('x1', x);
+            line.setAttribute('y1', puntoInicialEjeY);
+            line.setAttribute('x2', x);
+            line.setAttribute('y2', puntoFinalEjeY);
+            line.setAttribute('stroke', 'red');
+            line.setAttribute('stroke-width', '0.75');
+            this.grafico.append(line);
+        }
+
+        this.creaLabelX(step, xPrimerLinea, this.objGrafico.cantVerticalLines);
+    }
+
+    creaLabelX(step, xPrimerLinea, cantLabel) {
+
+        for (let i = 0; i < cantLabel; i++) {
+
+            let x = i * step + xPrimerLinea - step;
+
+            let text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+            text.setAttribute('x', x);
+            text.setAttribute('y', this.objGrafico.svgHeight);
+            text.setAttribute('fill', 'black');
+            text.setAttribute('font-size', '10');
+            let strTransform = "rotate(-45 " + x + "," + this.objGrafico.svgHeight + ")";
+            text.setAttribute('transform', strTransform);
+            text.innerHTML = `${this.objGrafico.xLabelData[i]}`;
+            this.grafico.append(text);
+        }
+    }
+
+    creaLineasHorizontal() {
+
+        let step = (this.objGrafico.areaGraficoHeight - (2 * this.objGrafico.margenCeroY)) / (this.objGrafico.cantHorizontalLines-1);
+        let puntoInicialEjeX = ((this.objGrafico.svgWidth - this.objGrafico.areaGraficoWidth) / 2) + (this.objGrafico.borderWidth / 2);
+        let puntoFinalEjeX = (this.objGrafico.svgWidth - this.objGrafico.areaGraficoWidth) / 2 + this.objGrafico.areaGraficoWidth - (this.objGrafico.borderWidth / 2);
+
+        let yPrimerLinea = this.objGrafico.svgHeight - ((this.objGrafico.svgHeight - this.objGrafico.areaGraficoHeight) / 2) - this.objGrafico.margenCeroX;
+
+        for (let i = 0; i < this.objGrafico.cantHorizontalLines; i++) {
+
+            let y = yPrimerLinea - i * step ;
+
+            let line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+            line.setAttribute('x1', puntoInicialEjeX);
+            line.setAttribute('y1', y);
+            line.setAttribute('x2', puntoFinalEjeX);
+            line.setAttribute('y2', y);
+            line.setAttribute('stroke', 'red');
+            line.setAttribute('stroke-width', '0.75');
+            this.grafico.append(line);
+
+            this.creaLabelY(step, yPrimerLinea, this.objGrafico.cantHorizontalLines);
+        }
+    }
+
+    creaLabelY(step, yPrimerLinea, cantLabel) {
+
+        for (let i = 0; i < cantLabel; i++) {
+
+            let y =  yPrimerLinea - i * step;
+
+            let text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+            text.setAttribute('x', 0);
+            text.setAttribute('y', y);
+            text.setAttribute('fill', 'black');
+            text.setAttribute('font-size', '10');
+            text.innerHTML = `${this.objGrafico.yLabelData[i]}`;
+            this.grafico.append(text);
+        }
+    }
+
+    /* actualiza valores */
+    graficaPuntos(mediciones) {
+
+        /* Colocar Valores */
+        let xStep = (this.objGrafico.areaGraficoWidth - (2 * this.objGrafico.margenCeroX)) / (this.objGrafico.cantVerticalLines-1);
+        let xPrimerLinea = ((this.objGrafico.svgWidth - this.objGrafico.areaGraficoWidth) / 2) + this.objGrafico.margenCeroX;
+        let xResolucion = this.objGrafico.xResolucion;
+
+        let yStep = (this.objGrafico.areaGraficoHeight - (2 * this.objGrafico.margenCeroY)) / (this.objGrafico.cantHorizontalLines-1);
+        let yPrimerLinea = this.objGrafico.svgHeight - ((this.objGrafico.svgHeight - this.objGrafico.areaGraficoHeight) / 2) - this.objGrafico.margenCeroX;
+        let yResolucion = this.objGrafico.yResolucion;
+
+        /* 
+           Busco el primer instante de medición. Es decir el valor de tiempo más chico.
+           A partir de ello encuentro el día que inician las mediciones y ese será la 
+           referencia del gráfico.
+        */
+
+        let timePoints = mediciones.map((el) => el.time);
+        let timeMin = Math.min(...timePoints);
+
+        let minDate = new Date(mediciones.find((el) => el.time.getTime() == timeMin).getTime());
+        
+        minDate.setHours(0);
+        minDate.setMinutes(0);
+        minDate.setSeconds(0);
+        minDate.setMilliseconds(0);
+        
+        let labelDate = mediciones.find((el) => el.time.getTime() == timeMin).getTime();
+
+        mediciones.forEach((el) => {
+
+            let origin = minDate.getTime() + (this.objGrafico.xResolucion * (this.objGrafico.cantVerticalLines-1)) * this.objGrafico.offset;
+
+            let xPos = ((xStep * (el.time.getTime() - origin)) / xResolucion) + xPrimerLinea;
+            let yPos =  yPrimerLinea - ((yStep * el.value) / yResolucion);
+            
+            let line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+            line.setAttribute('x1', xPos);
+            line.setAttribute('y1', yPrimerLinea);
+            line.setAttribute('x2', xPos);
+            line.setAttribute('y2', yPos);
+            line.setAttribute('stroke', 'blue');
+            line.setAttribute('stroke-width', '0.75');
+            grafico.append(line);
+
+            let circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+            circle.setAttribute('cx', xPos);
+            circle.setAttribute('cy', yPos);
+            circle.setAttribute('r', '3');
+            circle.setAttribute('stroke-width', '0.5');
+            circle.setAttribute('stroke', 'black');
+            circle.setAttribute('fill', 'blue');
+            grafico.append(circle);
+        });
+
+        this.actualizaTitulo(labelDate);
+    }
+
+    actualizaTitulo(minDate) {
+
+        let auxDate = new Date();
+        
+        // para aumentar o decrementar la fecha que aparece en el título lo hace en milisegundos.
+        auxDate.setTime(minDate.getTime() + this.objGrafico.offset*(3600000*24));
+
+        let dia  = auxDate.getDate();
+        let mes  = auxDate.getMonth() + 1;
+        let anio = auxDate.getFullYear();
+       
+        let title = document.getElementById(objGraficoValores.tituloID);
+        title.innerHTML = `Grafico Mediciones del día ${dia}/${mes}/${anio}`;
+
+    }
+
+    actualizaGrafico(mediciones) {
+
+        grafico.innerHTML = ``;
+    
+        this.creaAreaGrafico();
+        this.creaLineasVertical();
+        this.creaLineasHorizontal();
+        this.graficaPuntos(mediciones);
+    }
+
+    btnSiguiente(mediciones) {
+        this.objGrafico.offset = this.objGrafico.offset + 1;
+        this.actualizaGrafico(mediciones);
+    }
+
+    btnAnterior(mediciones) {
+        this.objGrafico.offset = this.objGrafico.offset - 1;
+        this.actualizaGrafico(mediciones);        
+    }
 }
 
-/* Creo Tabla HTML */
-/* Genero la tabla */
-let tabla = document.createElement("table");
-tabla.className = "tabla";
-tabla.id = "tablaValores";
+/* Objeto para gráfico */
+let objGraficoValores = {
+    svgID: 'grafico',
+    svgWidth: 500, // pixeles.
+    svgHeight: 500, // pixeles. 
+    svgViewBox: '0 0 500 500',
+    areaGraficoWidth: 450, // pixeles.
+    areaGraficoHeight: 450, // pixeles.
+    borderWidth: 5, // pixeles.
+    margenCeroX: 10, // pixeles.
+    margenCeroY: 10, // pixeles.
+    cantVerticalLines: 25,
+    cantHorizontalLines: 45,
+    xLabelData: '',
+    xResolucion: 3600000,
+    yLabelData: '',
+    yResolucion: 10,
+    tituloID: 'tituloGrafico',
+    offset: 0
+}
+
+objGraficoValores.xLabelData = [];
+for (let i = 0; i< objGraficoValores.cantVerticalLines ; i++) { objGraficoValores.xLabelData.push(i+":00"); }
+
+objGraficoValores.yLabelData = [];
+for (let i = 0; i< objGraficoValores.cantHorizontalLines; i++) { objGraficoValores.yLabelData.push(i*10); }
+
+let medGrafico = new Grafico(objGraficoValores);
+
+/* Asigno event listener a los botones Anterior y siguiente. */
+let btnSiguiente = document.getElementById('btnSiguiente');
+btnSiguiente.addEventListener('click',fBtnSiguiente);
+
+let btnAnterior = document.getElementById('btnAnterior');
+btnAnterior.addEventListener('click',fBtnAnterior);
+
+
 
 /* Creo usuario por defecto */
 const mUsuario = new Usuario(USER, PASS);
 let login = false;
-
 
 /* Oculta el contenido de la página y solo muestra el login */
 let secInputData = document.getElementById("inputData");
@@ -149,7 +332,7 @@ function loginEvt(e) {
         secInputData.style.display = "flex";
         secMuestraData.style.display = "flex";
 
-        actualizaTabla();
+        getMedPrev();
 
     } else {
 
@@ -158,10 +341,32 @@ function loginEvt(e) {
     }
 
     console.log("Login Exitoso");
-
-
-
 }
+
+/* Arrays para almacenar los valores previos y los que serán leídos por promt. */
+const mediciones = [];
+
+/* Lee de un archivo JSON mediciones previas */
+const getMedPrev = async () => {
+    const resp = await fetch('/json/mediciones.json');
+    const data = await resp.json();
+
+    /* Cargo el arreglo previo */
+    for (const med of data.mediciones) {
+        mediciones.push(new Medicion(med));
+    }
+
+    // Actualizo tabla
+    actualizaTabla(mediciones);
+    // Actualiza tabla resumen
+    let est = new EstMediciones(mediciones);
+    actualizaTablaResumen(est);
+    // Actualiza tabla menores de 220
+    tablaMenores220(mediciones);
+    // Actualiza grafico
+    medGrafico.actualizaGrafico(mediciones);
+}
+
 
 /* Control de formulario para ingreso manual de los datos */
 let inputInstante = document.getElementById("inputInstante");
@@ -174,7 +379,7 @@ formCargaManual.addEventListener("submit", cargaEvt);
 function cargaEvt(e) {
 
     e.preventDefault();
-    let time = inputInstante.value;
+    let time = new Date(inputInstante.value);
     let tension = inputValor.value;
     let unit = inputUnidad.value;
 
@@ -182,19 +387,38 @@ function cargaEvt(e) {
     // Agrego el objeto al array de mediciones.
     mediciones.push(med);
     //  Almaceno mediciones.
-    localStorage.setItem("mediciones", JSON.stringify(mediciones))
+    localStorage.setItem("mediciones", JSON.stringify(mediciones));
     // Actualiza tabla
-    actualizaTabla();
+    actualizaTabla(mediciones);
+    // Actualiza tabla resumen
+    let est = new EstMediciones(mediciones);
+    actualizaTablaResumen(est);
+    // Actualiza tabla menores de 220
+    tablaMenores220(mediciones);
+    // Actualiza grafico
+    medGrafico.actualizaGrafico(mediciones);
+
 
     Swal.fire({
-        title:'Nueva mediciòn cargada',
+        title: 'Nueva mediciòn cargada',
         icon: 'success',
         timer: 1500,
-        showConfirmButton:false       
-    }); 
+        showConfirmButton: false
+    });
+}
+
+function fBtnSiguiente(e) {
+
+    medGrafico.btnSiguiente(mediciones);
 
 }
 
+
+function fBtnAnterior(e) {
+
+    medGrafico.btnAnterior(mediciones);
+
+}
 
 class EstMediciones {
     constructor(objMediciones) {
@@ -242,34 +466,8 @@ class EstMediciones {
     }
 }
 
-
-let est = new EstMediciones(mediciones);
-
-
-/* Muestro por consola el resumen de los resultados */
-
-console.log("=====================================================================================");
-console.log("                               RESUMEN DE MEDICIONES                                 ");
-console.log("=====================================================================================");
-console.log("El promedio de los valores es " + est.getPromedio() + " voltios.");
-console.log("La medición de MAYOR valor fue: " + est.getMaximo().getValue() + " voltios y ocurrió a las " + est.getMaximo().getTime());
-console.log("La medición de MENOR valor fue: " + est.getMinimo().getValue() + " voltios y ocurrió a las " + est.getMinimo().getTime());
-
-let rango = est.getMaximo().getValue() - est.getMinimo().getValue();
-console.log("La diferencia de tensión entre el valor máximo y mínimo fue de: " + rango + "voltios");
-
-
-/* Se muestran por consola los valores de tensión que se encuentran por debajo de 220v */
-let menores220 = mediciones.filter((el) => el.getValue() < 220);
-
-console.log("Los Valores por debajo de 220v son: ");
-console.log("Instante       Valor de Tensión    Unidad");
-console.log("=========================================");
-menores220.forEach((el) => console.log(el.getTime() + "     " + el.getValue() + "      " + el.getUnit()));
-
-
-/* Vinculación con html */
-function actualizaTabla() {
+/* Vinculación con HTML tabla */
+function actualizaTabla(mediciones) {
     /* Genero la tabla */
     let tabla = document.getElementById("tablaValores");
     tabla.innerHTML = ``;
@@ -288,7 +486,7 @@ function actualizaTabla() {
         let filaMed = document.createElement("tr");
         filaMed.className = "filasValores";
         filaMed.innerHTML = `
-    <td>${med.getTime()}</td>
+    <td>${med.getTime().toLocaleString()}</td>
     <td>${med.getValue()}</td>
     <td>${med.getUnit()}</td>`;
 
@@ -296,15 +494,105 @@ function actualizaTabla() {
     }
 }
 
-// AJAX y API
+function actualizaTablaResumen(est) {
+    /* Genero la tabla */
+    let tabla = document.getElementById("tablaResumen");
+    tabla.innerHTML = ``;
 
+    // Genero el encabezado de la tabla.
+    let tablaTr = document.createElement("tr");
+    tablaTr.className = "tablaHeader";
+    tablaTr.innerHTML = `
+    <td>TABLA RESUMEN</td>`;
+    tabla.append(tablaTr);
+
+    let fila1 = document.createElement("tr");
+    fila1.className = "filasValores";
+    fila1.innerHTML = `<td>El promedio de los valores es ${est.getPromedio()} voltios.</td>`;
+    tabla.append(fila1);
+
+    let fila2 = document.createElement("tr");
+    fila2.className = "filasValores";
+    fila2.innerHTML = `<td>La medición de MAYOR valor fue: ${est.getMaximo().getValue()} voltios y ocurrió a las ${est.getMaximo().getTime()}</td>`;
+    tabla.append(fila2);
+
+    let fila3 = document.createElement("tr");
+    fila3.className = "filasValores";
+    fila3.innerHTML = `<td>La medición de MENOR valor fue: ${est.getMinimo().getValue()} voltios y ocurrió a las ${est.getMinimo().getTime()}</td>`;
+    tabla.append(fila3);
+}
+
+function tablaMenores220(mediciones) {
+    /* Genero la tabla */
+    let tabla = document.getElementById("tablaMenores220");
+    tabla.innerHTML = ``;
+
+    // Genero el encabezado de la table.
+    let tablaTr = document.createElement("tr");
+    tablaTr.className = "tablaHeader";
+    tablaTr.innerHTML = `
+    <td>Instante</td>
+    <td>Valor Tensión</td>
+    <td>Unidad</td>`;
+    tabla.append(tablaTr);
+
+    let menores220 = mediciones.filter((el) => el.getValue() < 220);
+    menores220.forEach((el) => {
+        let filaMed = document.createElement("tr");
+        filaMed.className = "filasValores";
+        filaMed.innerHTML = `
+        <td>${el.getTime().toLocaleString()}</td>
+        <td>${el.getValue()}</td>
+        <td>${el.getUnit()}</td>`;
+
+        tabla.append(filaMed);
+    });
+}
+
+
+// AJAX y API
 const pedirPrecioTarifa = async () => {
     const resp = await fetch('https://api.preciodelaluz.org/v1/prices/max?zone=PCB')
     const data = await resp.json()
 
-   console.log("El precio de la tarifa es:"+data.price+" "+data.units);  
-   
-    
+    console.log("El precio de la tarifa es:" + data.price + " " + data.units);
 }
 
 pedirPrecioTarifa();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
